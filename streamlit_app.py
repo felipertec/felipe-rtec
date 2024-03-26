436b8d58fd4c6caf84cb7bd75a81e9d46d3dfd50
felipertec <felipe.rtec@gmail.com>
2024-03-26 16:10:55 +0000

    primeiro commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.write('Welcome to Streamlit')
+st.write("Ola Mundo Primeiro Teste em Streamlit")
+
+# df = pd.DataFrame({
+#     'first colum': [1,2,3,4]
+#     'second collumn': [10,20,30,40]
+# })
+
+
